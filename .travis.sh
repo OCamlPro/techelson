0c803101c2dd3708debbacd2b2bb22f33ac6428b
AdrienChampion <adrien.champion@email.com>
2019-02-25 16:11:53 +0100

    trying to fix travis/osx

--- .travis.sh
+++ .travis.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
-sudo apt-get update -qq
-sudo apt-get install -y -qq libgmp-dev pandoc # ocaml ocaml-native-compilers
+if [[ "$TRAVIS_OS_NAME" == "linux" ]] ; then
+    sudo apt-get update -qq
+    sudo apt-get install -y -qq libgmp-dev pandoc # ocaml ocaml-native-compilers
 
-# do this in a second step to only install libsecp256k1-dev libsecp256k1-0
-# for ubuntu, these packages are not available in trusty
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu artful main universe"
-sudo apt-get update -qq
-sudo apt-get install -y -qq \
-     libsecp256k1-dev libsecp256k1-0 libsodium-dev libssl-dev \
-     bubblewrap libev-dev libhidapi-dev
+    # do this in a second step to only install libsecp256k1-dev libsecp256k1-0
+    # for ubuntu, these packages are not available in trusty
+    sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu artful main universe"
+    sudo apt-get update -qq
+    sudo apt-get install -y -qq \
+         libsecp256k1-dev libsecp256k1-0 libsodium-dev libssl-dev \
+         bubblewrap libev-dev libhidapi-dev
+fi
 
 export OPAMYES=1
 wget https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh
