fb237d052cb80b332722529692a59ce2e36d9fa7
AdrienChampion <adrien.champion@email.com>
2019-02-25 10:38:12 +0100

    fixing travis

--- .travis.sh
+++ .travis.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
+export OPAMYES=1
+wget https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh
+yes "" | sh install.sh
+
+opam switch create techelson 4.07.1
 
 eval $(opam config env)
+
 opam update
-eval $(opam config env)
-opam switch create techelson 4.07.1
-eval $(opam config env)
 opam install dune menhir zarith ptime
-eval $(opam config env)
 
 opam --version
 
