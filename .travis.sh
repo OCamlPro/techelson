d172d298b065070614c65d8cabb2d529ff289ed4
AdrienChampion <adrien.champion@email.com>
2019-02-25 10:40:45 +0100

    fixing travis

--- .travis.sh
+++ .travis.sh
@@ -4,6 +4,12 @@ export OPAMYES=1
 wget https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh
 yes "" | sh install.sh
 
+eval $(opam config env)
+
+opam init
+
+eval $(opam config env)
+
 opam switch create techelson 4.07.1
 
 eval $(opam config env)
